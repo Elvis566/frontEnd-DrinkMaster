2e1adb8ad9f6cf02c3a9187e5c1b5877e5305254
RobinsonZam025 <robinsonzambranos1@hotmail.com>
2024-08-14 09:11:16 -0500

    ErrorPasword1

--- Interfaz/src/app/LoginRegister/login/login.page.ts
+++ Interfaz/src/app/LoginRegister/login/login.page.ts
@@ -10,35 +10,26 @@ import { AlertController } from '@ionic/angular';
 })
 export class LoginPage  {
 
+  msg:any;
+
   constructor( private apiS: ApiNodeService, private router:Router,
               private alertController: AlertController) { }
 
   login(email:any, password:any){
     this.apiS.loginUser(email.value, password.value).subscribe({
       next: (datos:any)=>{
+        this.msg = datos.message
 
         localStorage.setItem('apodo', datos.user.apodo)
         localStorage.setItem('foto', datos.user.avatar_id)
         localStorage.setItem('idUser', datos.user.id)
 
         this.router.navigate(['/tabs-inicio'])
-        this.presentAlert('Login Failed', 'Incorrect email or password.');
       },
       error:(e:any)=>{
         debugger
         console.log(e);
       }
     })
-  }
-
-  async presentAlert(header: string, message: string) {
-    const alert = await this.alertController.create({
-      header,
-      message,
-      buttons: ['OK']
-    });
-
-    await alert.present();
-
   }
 }
